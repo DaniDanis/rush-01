797fe345bd291c6f056e91694464a72a0f34f407
Daniel Dantas Da Silva <dadantas@c1r10p4.42sp.org.br>
2023-08-27 14:06:55 -0300

    Mudanças simples

--- ex00/main.c
+++ ex00/main.c
@@ -6,14 +6,14 @@
 /*   By: dadantas <dadantas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 14:07:14 by dadantas          #+#    #+#             */
-/*   Updated: 2023/08/27 12:44:57 by dadantas         ###   ########.fr       */
+/*   Updated: 2023/08/27 12:52:54 by dadantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_check_param(char *param);
 void	validation_game(char *input);
+int		ft_check_param(char *param);
 
 int	ft_print_error(void)
 {
