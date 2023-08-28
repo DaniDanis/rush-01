2de1df5b8c4fbcc4e6726f457f881faf8d6ca540
Daniel Dantas Da Silva <dadantas@c2r2p1.42sp.org.br>
2023-08-27 21:42:58 -0300

    Adiciona condicional de error

--- ex00/ft_print_matrix.c
+++ ex00/ft_print_matrix.c
@@ -6,13 +6,37 @@
 /*   By: dadantas <dadantas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 05:38:48 by caredua3          #+#    #+#             */
-/*   Updated: 2023/08/27 21:23:55 by dadantas         ###   ########.fr       */
+/*   Updated: 2023/08/27 21:42:34 by dadantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 int	ft_print_error(void);
+
+int	ft_print_error_matrix(int matrix[4][4])
+{
+	int	line;
+	int	column;
+	int	sum;
+
+	line = 0;
+	sum = 0;
+	while (line < 4)
+	{
+		column = 0;
+		while (column < 4)
+		{
+			sum += matrix[line][column];
+			column++;
+		}
+		if (sum > 10)
+			return (1);
+		line++;
+		sum = 0;
+	}
+	return (0);
+}
 
 int	ft_print_matrix(int matrix[4][4])
 {
@@ -22,6 +46,8 @@ int	ft_print_matrix(int matrix[4][4])
 
 	line = 0;
 	collumn = 0;
+	if (ft_print_error_matrix(matrix))
+		return (ft_print_error());
 	while (line < 4)
 	{
 		collumn = 0;
